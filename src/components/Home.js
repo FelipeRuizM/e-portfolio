c5e106c81136e1de204e0f36ea8be231fae0b4ca
Felipe Ruiz <zatuchi01@gmail.com>
2023-09-03 16:29:57 -0700

    added the toasty! easter egg

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap";
 export const Home = () => {
 
   return (
-    <section id='home'>.
+    <section id='home'>Toasty!
       <Row className="justify-content-md-center align-items-center" id="home-row">
         <Col className="col-lg-9 p-4">
           <h4 className="style-text-sub-color text-start">Hi, my name is</h4>
