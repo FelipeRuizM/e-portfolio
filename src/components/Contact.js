6d655afedd59ec9cde930598edf4866657bcf343
Felipe Ruiz <zatuchi01@gmail.com>
2023-09-05 23:47:54 -0700

    fixed contact 'button' message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,7 @@ export const Contact = () => {
         <Col className="col-lg-4">
           <h3 style={{marginTop: '3rem'}} className="style-text-main-color display-4">Get in Touch</h3>
           <p style={{marginBottom: '5rem'}} className="style-text-text-color">Hey, I'm currently looking for new opportunities. Even if you just have a question or wanna connect, please feel free to do so. I'm a nice guy, I swear! :D</p>
-          <a id="contact-button" className="float-center" href="mailto: felipe.ruiz0011@gmail.com">Get to know me &gt;</a>
+          <a id="contact-button" className="float-center" href="mailto: felipe.ruiz0011@gmail.com">Say Hello &gt;</a>
         </Col>
       </Row>
     </section>
