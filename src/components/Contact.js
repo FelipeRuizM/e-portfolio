69a473eb61901fa435576b0bf7d1682989b69628
Felipe Ruiz <zatuchi01@gmail.com>
2023-09-05 23:45:54 -0700

    contact section done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,13 @@ export const Contact = () => {
             <span className='style-text-sub-color'>3.</span> Contact</h3>
         </Col>
       </Row>
+      <Row className="justify-content-md-center align-items-center">
+        <Col className="col-lg-4">
+          <h3 style={{marginTop: '3rem'}} className="style-text-main-color display-4">Get in Touch</h3>
+          <p style={{marginBottom: '5rem'}} className="style-text-text-color">Hey, I'm currently looking for new opportunities. Even if you just have a question or wanna connect, please feel free to do so. I'm a nice guy, I swear! :D</p>
+          <a id="contact-button" className="float-center" href="mailto: felipe.ruiz0011@gmail.com">Get to know me &gt;</a>
+        </Col>
+      </Row>
     </section>
   );
 }
